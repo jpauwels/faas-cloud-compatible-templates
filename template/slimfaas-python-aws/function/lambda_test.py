6f15fa3645b4dba5628d60fc3f05ec65c2686426
Johan Pauwels <jpauwels@users.noreply.github.com>
2024-09-29 16:37:33 +0100

    First version of AWS-compatible Python templates

---
+++ template/slimfaas-python-aws/function/lambda_test.py
@@ -0,0 +1,10 @@
+from .lambda_function import lambda_handler
+
+# Test your handler here
+
+# To disable testing, you can set the build_arg `TEST_ENABLED=false` on the CLI or in your stack.yml
+# https://docs.openfaas.com/reference/yaml/#function-build-args-build-args
+
+def test_handler():
+    # assert lambda_handler("input") == "input"
+    pass
